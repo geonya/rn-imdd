9f95bf7471b0109be38a581a4bbbb53ca0ec76fc
geonya <geony8410@gmail.com>
2022-03-30 13:13:46 +0900

    # 3.2 Movies Screen part One

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,23 +1,41 @@
 import React from "react";
 import styled from "styled-components/native";
+import { Dimensions } from "react-native";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
+import Swiper from "react-native-web-swiper";
 
-const Btn = styled.TouchableOpacity`
-	flex: 1;
-	justify-content: center;
-	align-items: center;
+const API_KEY = "0d22bba8e2d67dad7a9bfff6e35b1d56";
+const nowPlayingUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`;
+
+const Container = styled.ScrollView`
 	background-color: ${(props) => props.theme.mainBgColor};
 `;
-const Title = styled.Text`
-	color: ${(props) => props.theme.textColor};
+
+const View = styled.View`
+	flex: 1;
 `;
 
-const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
-	navigation,
-}) => (
-	<Btn onPress={() => navigation.navigate("Stack", { screen: "Three" })}>
-		<Title>Movies</Title>
-	</Btn>
-);
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+	const getNowPlaying = () => {
+		fetch(nowPlayingUrl);
+	};
+	return (
+		<Container>
+			<Swiper
+				loop
+				timeout={3.5} // autoplay
+				controlsEnabled={false}
+				containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+			>
+				<View style={{ backgroundColor: "red" }}></View>
+				<View style={{ backgroundColor: "blue" }}></View>
+				<View style={{ backgroundColor: "red" }}></View>
+				<View style={{ backgroundColor: "blue" }}></View>
+			</Swiper>
+		</Container>
+	);
+};
 
 export default Movies;

d0fd1e1c372069c7f1a9152288219506705d7b67
geonya <geony8410@gmail.com>
2022-03-30 15:08:25 +0900

    # 3.4 Movies Screen part Three styled extends

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
-import { ActivityIndicator, Dimensions, StyleSheet } from "react-native";
+import {
+	ActivityIndicator,
+	Dimensions,
+	StyleSheet,
+	useColorScheme,
+} from "react-native";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import Swiper from "react-native-swiper";
 import { BlurView } from "expo-blur";
@@ -22,12 +27,42 @@ const Loader = styled.View`
 `;
 
 const BgImg = styled.Image``;
+const Poster = styled.Image`
+	width: 100px;
+	height: 160px;
+	border-radius: 5px;
+`;
 
-const Title = styled.Text``;
+const Title = styled.Text`
+	font-size: 16px;
+	font-weight: 600;
+	color: ${(props) => props.theme.textColor};
+`;
+
+const Overview = styled.Text`
+	margin-top: 10px;
+	opacity: 0.8;
+	color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.View`
+	flex-direction: row;
+	height: 100%;
+	justify-content: center;
+	align-items: center;
+`;
+const Column = styled.View`
+	width: 40%;
+	margin-left: 15px;
+`;
+const Votes = styled(Overview)`
+	font-size: 12px;
+`;
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+	const isDark = useColorScheme() === "dark";
 	const [loading, setLoading] = useState(true);
 	const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
 	const getNowPlaying = async () => {
@@ -46,8 +81,10 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
 	) : (
 		<Container>
 			<Swiper
+				horizontal
 				loop
 				autoplayTimeout={3.5} // autoplay
+				showsButtons={false}
 				showsPagination={false}
 				containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
 			>
@@ -58,10 +95,28 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
 							style={StyleSheet.absoluteFill}
 						/>
 						<BlurView
-							intensity={10}
+							intensity={80}
 							style={StyleSheet.absoluteFill}
+							tint={isDark ? "dark" : "light"}
 						>
-							<Title>{movie.original_title}</Title>
+							<Wrapper>
+								<Poster
+									source={{
+										uri: makeImagePath(movie.poster_path),
+									}}
+								/>
+								<Column>
+									<Title>{movie.original_title}</Title>
+									{movie.vote_average > 0 ? (
+										<Votes>
+											⭐️{movie.vote_average}/10
+										</Votes>
+									) : null}
+									<Overview>
+										{movie.overview?.slice(0, 90)}...
+									</Overview>
+								</Column>
+							</Wrapper>
 						</BlurView>
 					</View>
 				))}

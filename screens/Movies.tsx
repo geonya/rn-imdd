fa14c5e4db069a4143827f247e359d856d20e8f5
geonya <geony8410@gmail.com>
2022-03-31 11:21:54 +0900

    3.7 Coming Soon

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -7,9 +7,9 @@ import Slide from "../components/Slide";
 import Poster from "../components/Poster";
 
 const API_KEY = "0d22bba8e2d67dad7a9bfff6e35b1d56";
-const nowPlayingUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&region=KR`;
-const upComingUrl = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&region=KR`;
-const trendingUrl = `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}&region=KR`;
+const nowPlayingUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}`;
+const upComingUrl = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}`;
+const trendingUrl = `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`;
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Container = styled.ScrollView``;
@@ -17,6 +17,9 @@ const Loader = styled.View`
 	flex: 1;
 	justify-content: center;
 	align-items: center;
+`;
+const ListContainer = styled.View`
+	margin-bottom: 40px;
 `;
 const ListTitle = styled.Text`
 	color: ${(props) => props.theme.textColor};
@@ -41,6 +44,32 @@ const Votes = styled.Text`
 	opacity: 0.8;
 	color: ${(props) => props.theme.textColor};
 `;
+const HMovie = styled.View`
+	padding: 0px 30px;
+	flex-direction: row;
+	margin-bottom: 30px;
+`;
+
+const HColumn = styled.View`
+	margin-left: 15px;
+	width: 80%;
+`;
+
+const Overview = styled.Text`
+	color: ${(props) => props.theme.textColor};
+	width: 80%;
+`;
+
+const ComingSoonTitle = styled(ListTitle)`
+	margin-bottom: 20px;
+`;
+
+const Release = styled.Text`
+	color: ${(props) => props.theme.textColor};
+	font-size: 12px;
+	margin-bottom: 5px;
+`;
+
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
 	const [loading, setLoading] = useState(true);
 	const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
@@ -95,23 +124,56 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
 					/>
 				))}
 			</Swiper>
-			<ListTitle>Trending Movies</ListTitle>
-			<TrendingScroll
-				contentContainerStyle={{ paddingLeft: 30 }}
-				horizontal
-				showsHorizontalScrollIndicator={false}
-			>
-				{trendingMovies.map((movie) => (
-					<Movie key={movie.id}>
-						<Poster path={movie.poster_path} />
-						<Title>
-							{movie.original_title.slice(0, 12)}
-							{movie.original_title.length > 13 ? "..." : null}
-						</Title>
-						<Votes>⭐️ {movie.vote_average}/10</Votes>
-					</Movie>
-				))}
-			</TrendingScroll>
+			<ListContainer>
+				<ListTitle>Trending Movies</ListTitle>
+				<TrendingScroll
+					contentContainerStyle={{ paddingLeft: 30 }}
+					horizontal
+					showsHorizontalScrollIndicator={false}
+				>
+					{trendingMovies.map((movie) => (
+						<Movie key={movie.id}>
+							<Poster path={movie.poster_path} />
+							<Title>
+								{movie.original_title.slice(0, 12)}
+								{movie.original_title.length > 13
+									? "..."
+									: null}
+							</Title>
+							<Votes>
+								{movie.vote_average > 0
+									? `⭐️ ${movie.vote_average}/10`
+									: "Coming Soon"}
+							</Votes>
+						</Movie>
+					))}
+				</TrendingScroll>
+			</ListContainer>
+			<ComingSoonTitle>Coming Soon</ComingSoonTitle>
+			{upcomingMovies.map((movie) => (
+				<HMovie key={movie.id}>
+					<Poster path={movie.poster_path} />
+					<HColumn>
+						<Title>{movie.original_title}</Title>
+						<Release>
+							{new Date(movie.release_date).toLocaleDateString(
+								"ko",
+								{
+									month: "long",
+									day: "numeric",
+									year: "numeric",
+								}
+							)}
+						</Release>
+						<Overview>
+							{movie.overview !== "" &&
+							movie.overview.length > 150
+								? `${movie.overview.slice(0, 150)}...`
+								: movie.overview}
+						</Overview>
+					</HColumn>
+				</HMovie>
+			))}
 		</Container>
 	);
 };

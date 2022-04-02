e50479e93650a1ebfd7756231e8c4eb256a6c911
geonya <geony8410@gmail.com>
2022-04-02 14:44:29 +0900

    #3.26 Sharing

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,7 +1,7 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { LinearGradient } from "expo-linear-gradient";
 import React, { useEffect } from "react";
-import { StyleSheet, Linking } from "react-native";
+import { StyleSheet, TouchableOpacity, Share, Platform } from "react-native";
 import { useQuery } from "react-query";
 import styled from "styled-components/native";
 import { Movie, moviesApi, TV, tvApi } from "../api";
@@ -63,10 +63,39 @@ const Detail: React.FC<DetailScreenProps> = ({
 	route: { params },
 }) => {
 	const isMovie = "original_title" in params;
+
 	const { isLoading, data } = useQuery(
 		[isMovie ? "movies" : "tv", params.id],
 		//@ts-ignore
 		isMovie ? moviesApi.detail : tvApi.detail
+	);
+	const shareMedia = async () => {
+		const isAndroid = Platform.OS === "android";
+		const homepage = isMovie
+			? `https://imdb.com/title/${data.imdb_id}`
+			: data.homepage;
+		if (isAndroid) {
+			await Share.share({
+				message: `${params.overview}\n Check it out: ${homepage}`,
+				title:
+					"original_title" in params
+						? params.original_title
+						: params.original_name,
+			});
+		} else {
+			await Share.share({
+				url: homepage,
+				title:
+					"original_title" in params
+						? params.original_title
+						: params.original_name,
+			});
+		}
+	};
+	const ShareBtn = () => (
+		<TouchableOpacity onPress={shareMedia}>
+			<Ionicons name="share-outline" color="white" size={24} />
+		</TouchableOpacity>
 	);
 
 	useEffect(() => {
@@ -74,9 +103,18 @@ const Detail: React.FC<DetailScreenProps> = ({
 			title: "original_title" in params ? "Movie" : "TV Show",
 		});
 	}, []);
-	const openYTLink = async (videoId) => {
+	useEffect(() => {
+		if (data) {
+			setOptions({
+				headerRight: () => <ShareBtn />,
+			});
+		}
+	}, [data]);
+	const openYTLink = async (videoId: string) => {
 		const baseUrl = `http://m.youtube.com/watch?v=${videoId}`;
+		// await Linking.openURL(baseUrl);
 		await webBrowser.openBrowserAsync(baseUrl);
+		``;
 	};
 	return (
 		<Container>
